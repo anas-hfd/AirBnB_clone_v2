3cae6198a037c9ee0e2213642852a133f5703e17
anas-hfd <anas1.hfd@gmail.com>
2023-10-05 20:39:19 +0000

    dep

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 #distributes an archive to the web servers, using the function do_deploy:
 import os.path
-from fabric.api import env, run, put
+from fabric.api import env
+from fabric.api import run
+from fabric.api import put
 
 env.hosts = ["100.25.37.85", "100.26.228.112"]
 
