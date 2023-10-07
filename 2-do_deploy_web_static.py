4120a10375ea21b6586d36292886976105712cb7
anas-hfd <anas1.hfd@gmail.com>
2023-10-07 20:06:54 +0100

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,43 +1,31 @@
 #!/usr/bin/python3
 #distributes an archive to the web servers, using the function do_deploy:
-import os.path
-from fabric.api import env, put, run, local, task
+import os
+from fabric.api import *
 
 env.hosts = ["100.25.37.85", "100.26.228.112"]
 
 
-@task
-def do_pack():
-#1-pack_web_static.py do_pack
-
-    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
-    mkdir = "mkdir -p versions"
-    path = "versions/web_static_{}.tgz".format(formatted_dt)
-    print("Packing web_static to {}".format(path))
-    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
-        return path
-    return None
-
-
-@task
 def do_deploy(archive_path):
-	#Deploys an archive to a web server.
-	try:
-		if not os.path.exists(archive_path):
-			return False
-		fn_with_ext = os.path.basename(archive_path)
-		fn_no_ext, ext = os.path.splitext(fn_with_ext)
-		dpath = "/data/web_static/releases/"
-		put(archive_path, "/tmp/")
-		run("rm -rf {}{}/".format(dpath, fn_no_ext))
-		run("mkdir -p {}{}/".format(dpath, fn_no_ext))
-		run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
-		run("rm /tmp/{}".format(fn_with_ext))
-		run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
-		run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
-		run("rm -rf /data/web_static/current")
-		run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
-		print("New version deployed!")
-		return True
-	except Exception:
-		return False
+    #deploys the archive to the web servers
+    if not os.path.exists(archive_path):
+        return False
+
+    # Upload the archive to /tmp/ on the remote servers
+    put(archive_path, '/tmp/')
+
+    # Extract the archive to /data/web_static/releases/
+    archive_filename = os.path.basename(archive_path)
+    archive_name_no_ext = os.path.splitext(archive_filename)[0]
+    remote_path = f'/data/web_static/releases/{archive_name_no_ext}'
+    run(f'mkdir -p {remote_path}')
+    run(f'tar -xzf /tmp/{archive_filename} -C {remote_path}')
+    run(f'rm /tmp/{archive_filename}')
+
+    # Delete the old symbolic link
+    run('rm -f /data/web_static/current')
+
+    # Create a new symbolic link
+    run(f'ln -s {remote_path} /data/web_static/current')
+
+    return True
